ed7708f3f33cde5f8f9eda8e71bf1b689821e1c1
zooey <zooeyxmiller@gmail.com>
2020-11-08 21:54:03 +0000

    Create initial shuffling logic

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,6 +1,6 @@
 module Cards where
 
-import Data.Set
+import System.Random
 
 data Suit = Diamonds | Hearts | Spades | Clubs deriving (Eq, Show, Enum)
 
@@ -26,9 +26,31 @@ data Card = Card
   }
   deriving (Show)
 
-packOfCards :: Set Card
+-- can I specify unique list?
+packOfCards :: [Card]
 packOfCards =
-  fromDistinctAscList $ do
+  do
     suit <- [toEnum 0 ..]
     rank <- [toEnum 0 ..]
     pure $ Card suit rank
+
+shuffle :: [Card] -> IO [Card]
+shuffle cards = do
+  initialLength <- pure $ length cards - 1
+
+  -- get me a random index in the list
+  index <- getStdRandom $ randomR (0, initialLength)
+
+  -- get the card at that index, and the list with that card removed
+  let (card, newCards) = removeCard index cards
+
+  -- if the card existed, recurse while concatenating, else return just the newCards (an empty list at this point)
+  case card of
+    Just c -> fmap (c :) (shuffle newCards)
+    Nothing -> pure newCards
+
+removeCard :: Int -> [Card] -> (Maybe Card, [Card])
+removeCard i cs = go 0 i cs []
+  where
+    go curr i' (c : cs') rcs = if curr == i' then (Just c, cs' ++ rcs) else go (curr + 1) i' cs' (c : rcs)
+    go curr i' [] rcs = (Nothing, rcs)
